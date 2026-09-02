228c7a1edf64b8eed927d176462a60a988940728
ptylczynski <ptylczynski@man.poznan.pl>
2026-09-02 21:46:11 +0200

    refactor: add GTFS service exports

---
+++ gtfs/services/__init__.py
@@ -0,0 +1,12 @@
+from gtfs.services.downloader import (
+    GtfsDownloadBatchError,
+    GtfsDownloadService,
+)
+from gtfs.services.importer import GtfsImportService
+
+
+__all__ = (
+    'GtfsDownloadBatchError',
+    'GtfsDownloadService',
+    'GtfsImportService',
+)
