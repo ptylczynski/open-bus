3191bba9abc6f15a01718ccc36276a9c85b21bda
ptylczynski <ptylczynski@man.poznan.pl>
2026-09-02 23:50:49 +0200

    feat: expose route API endpoints

--- gtfs/urls.py
+++ gtfs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from gtfs.views import StopListView
+from gtfs.views import RouteCreateView, StopListView
 
 
 urlpatterns = [
+    path('routes/', RouteCreateView.as_view(), name='route-create'),
     path('stops/', StopListView.as_view(), name='stop-list'),
 ]
