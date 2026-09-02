7cad39e9063c31d4467d7e34b69543843da3ee0b
ptylczynski <ptylczynski@man.poznan.pl>
2026-09-03 00:11:21 +0200

    feat: add stop suggestion endpoint

--- gtfs/urls.py
+++ gtfs/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from gtfs.views import RouteCreateView, StopListView
+from gtfs.views import RouteCreateView, StopListView, StopSuggestionView
 
 
 urlpatterns = [
     path('routes/', RouteCreateView.as_view(), name='route-create'),
+    path(
+        'stops/suggest/',
+        StopSuggestionView.as_view(),
+        name='stop-suggest',
+    ),
     path('stops/', StopListView.as_view(), name='stop-list'),
 ]
